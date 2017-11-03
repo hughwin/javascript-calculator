3755e8723137d271dc8f867baed0fff65f349e8e
hughwin <hugh.winchester@gmail.com>
2017-11-03 11:41:51 +0000

    Javascript completely rewritten
    The old calculator sucked. This one works properly.

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/hughwin/pen/xEQOwm.
+A Pen created at CodePen.io. You can find this one at https://codepen.io/hughwin/pen/wJLxPR.
 
  
--- js/index.js
+++ js/index.js
@@ -1,167 +1,144 @@
-var calculation = "";
-$(".btn").mouseup(function() {
-  $(this).blur();
-});
-$("#decimal").click(function() {
-  checkLength();
-  if (checkPreceding()){
-    calculation += ".";
-    $("#output").html(calculation);
-  };
-});
-$("#left-bracket").click(function() {
-  checkLength();
-  calculation += "(";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#right-bracket").click(function() {
-  checkLength();
-  calculation += ")";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#zero").click(function() {
-  checkLength();
-  if (calculation[0] === "0" && calculation[1] !== ".") {
-    alert("Can't add anymore zeroes in front");
-  } else {
-    calculation += "0";
-    console.log(calculation);
-  }
-  $("#output").html(calculation);
-});
-$("#one").click(function() {
-  checkLength();
-  calculation += "1";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#two").click(function() {
-  checkLength();
-  calculation += "2";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#three").click(function() {
-  checkLength();
-  calculation += "3";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#four").click(function() {
-  checkLength();
-  calculation += "4";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#five").click(function() {
-  checkLength();
-  calculation += "5";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#six").click(function() {
-  checkLength();
-  calculation += "6";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#seven").click(function() {
-  checkLength();
-  calculation += "7";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#eight").click(function() {
-  checkLength();
-  calculation += "8";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#nine").click(function() {
-  checkLength();
-  calculation += "9";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#AC").click(function() {
-  calculation = "";
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#CE").click(function() {
-  calculation = calculation.substring(0, calculation.length - 1);
-  console.log(calculation);
-  $("#output").html(calculation);
-});
-$("#plus").click(function() {
-  checkLength();
-  if (checkPreceding("+")){
-  calculation += "+";
-  console.log(calculation);
-  $("#output").html(calculation);
-  }
-});
-$("#times").click(function() {
-  checkLength();
-  if (checkPreceding("*")){
-  calculation += "*";
-  console.log(calculation);
-  $("#output").html(calculation);
-  }
-});
-$("#divide").click(function() {
-  checkLength();
-  if (checkPreceding("/")){
-  calculation += "/";
-  console.log(calculation);
-  $("#output").html(calculation);
-  }
-});
-$("#subtract").click(function() {
-  checkLength();
-  if (calculation[calculation.length-1] === "-"){
-    alert("Stop trying to break my calculator!");
-  }
-  else{
-  calculation += "-";
-  console.log(calculation);
-  $("#output").html(calculation);
-  }
-});
-$("#equals").click(function compute() {
-  checkLength();
-  if (calculation[0] === "0" && calculation[1] !== ".") {
-    calculation = calculation.substring(1);
-  }
-  var final = eval(calculation);
-  console.log(final)
-  if (calculation == final){
-    console.log("error");
-    $("#output").html("Math error");
-  }
-  else{
-  $("#output").html(final); 
-  calculation = "";
-  }
-});
-function checkLength(){
-  if (calculation.length > 21){
-    compute();
-  }
-}
-function checkPreceding(operator){
-  if (calculation === "" && operator === "+" || calculation === "" && operator === "*"  || calculation === "" && operator === "/" || calculation === "" && operator === "+"){
-    alert("Stop trying to break my calculator!");
-    return false
-  }
-  if (calculation[calculation.length-1] === "/" || calculation[calculation.length-1] === "*" || calculation[calculation.length-1] === "-" || calculation[calculation.length-1] === "+" || calculation[calculation.length-1] === "."){
-      alert("Stop trying to break my calculator!");
-    return false
+$(document).ready(function() {
+  // Variables to hold all the information and states in.
+  var log = "",
+    current = "",
+    ans = "",
+    revertToNormal = false,
+    locked = false;
+
+  $("button").click(function() {
+    entry = $(this).attr("value");
+    console.log("entry: " + entry);
     
+
+    // Allows the use of a decimal point after using an operator.
+    if (locked === true && entry === ".") {
+      locked = false;
+      current = "";
+    }
+    // Checks whether numbers being supplied are valid I.e. not something like 000015
+
+    // If "locked" after an operator has been added, the calculator will not accept any entries apart from numbers.
+    if (locked === true && isNaN(entry) === true) {
+      log += "";
+      $("#output").html(current);
+      $("#log").html(log);
+      console.log("test current: " + current);
+    }
+
+    // Clear all operations
+    if (entry === "AC") {
+      current = "";
+      log = "";
+      ans = "";
+      $("#output").html(current);
+      $("#log").html(current);
+    }
+
+    // Clear last operation
+    if (entry === "CE") {
+      if (revertToNormal === true) {
+        current = "";
+        log = "";
+        ans = "";
+        $("#output").html(current);
+        $("#log").html(current);
       }
-  else {
-    return true
+      current = "";
+      $("#output").html(current);
+
+      // computes an answer.
+    } else if (entry === "=" && isNaN(entry) === true && current !== ans) {
+      log += current;
+      ans = eval(log);
+      console.log("ans: " + ans);
+      console.log(typeof ans);
+      //let arr = ans.split("")
+      if (ans % 1 !== 0) {
+        ans = Math.round(ans * 100) / 100;
+        console.log("New ans: " + ans);
+        $("#output").html(ans);
+        $("#log").html(log + "=");
+        current = ans;
+        log = "";
+        revertToNormal = true;
+      }
+      $("#output").html(ans);
+      $("#log").html(log + "=");
+      current = ans;
+      log = "";
+      revertToNormal = true;
+    }
+    if (entry === "*" && current.length !== 0 && locked === false) {
+      //multiplication
+      locked = true;
+      log += current + "*";
+      current = "*";
+      $("#output").html(current);
+      $("#log").html(log);
+    }
+
+    if (entry === "/" && current.length !== 0 && locked === false) {
+      //division
+      locked = true;
+      log += current + "/";
+      current = "/";
+      $("#output").html(current);
+      $("#log").html(log);
+    }
+
+    if (entry === "-" && current.length !== 0 && locked === false) {
+      //subtraction
+      locked = true;
+      log += current + "-";
+      current = "-";
+      $("#output").html(current);
+      $("#log").html(log);
+    }
+
+    if (entry === "+" && current.length !== 0 && locked === false) {
+      //addition
+      locked = true;
+      log += current + "+";
+      current = "+";
+      $("#output").html(current);
+      $("#log").html(log);
+    } else if (validNum(entry) === false) {
+      // Check valid numbers
+      entry += "";
+      current += "";
+      console.log("alternative current: " + current);
+
+      // Only way to unlock after an operator.
+    } else if (locked === true && isNaN(entry) === false) {
+      locked = false;
+      current = "";
+      current += entry;
+      $("#output").html(current);
+    } else if (isNaN(entry) === false || entry === ".") {
+      if (revertToNormal === true) {
+        current = "";
+        log = "";
+        ans = "";
+        $("#output").html(current);
+        $("#log").html(log);
+        revertToNormal = false;
+      } else if (current.length < 16) {
+        current += entry;
+        $("#output").html(current);
+        console.log(current);
+      }
+      else {alert("You've entered too many numbers. Please use an operator.")}
+    }
+  });
+  // detects if number being entered is valid
+  function validNum(item) {
+    if (
+      (current === "0" && item !== ".") ||
+      (current.length === 0 && isNaN(entry) && entry !== ".") ||
+      (current[current.length - 1] === "." && entry === ".")
+    ) {
+      return false;
+    }
   }
-}
+});
